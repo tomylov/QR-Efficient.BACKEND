7faba7a575e47f1baa081e07ac59c1f95573e100
tomas arias <tomas.arias2001@gmail.com>
2024-10-04 09:16:10 -0300

    Add CORS middleware

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import api from './src/routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 app.use(express.json());
-app.use('/api',api);
+app.use('/api', api);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`App listening on port: ${port}`))
